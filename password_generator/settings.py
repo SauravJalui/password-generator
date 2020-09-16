461c4c7d685cfb1f4d2e423dfb885538d5d67a4a
SauravJalui <sauravjalui.007@gmail.com>
2020-09-16 21:45:28 +0530

    modified and removed the secret key

--- password_generator/settings.py
+++ password_generator/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '88@*h37llm6(&nt13eu98ys0ab)(1o=veqt_svp(5m(_#an_c*'
+SECRET_KEY = ' '
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
